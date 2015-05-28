21efda975ca2e4473d9932596ea32c56efe26c37
darylcalderon <daryl.calderon@hotmail.com>
2015-05-29 00:17:29 +1200

    Added eventList()

--- Scheduler/app/src/main/java/nz/ac/aut/sdp/scheduler/DayView.java
+++ Scheduler/app/src/main/java/nz/ac/aut/sdp/scheduler/DayView.java
@@ -1,38 +1,50 @@
 package nz.ac.aut.sdp.scheduler;
 
+import android.app.DatePickerDialog;
 import android.database.Cursor;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.method.DateTimeKeyListener;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 
 
 public class DayView extends ActionBarActivity {
 
-    List<Event> events;
+    ArrayList<Event> eventsToday;
+    ArrayAdapter<String> eventsAdapter;
     DBAdapter db;
+    EditText currentDay;
+    String date1;
+    Calendar myCalendar = Calendar.getInstance();
+    ListView listEvents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_view);
 
+        dateChange();
 
-        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        Date date = new Date();
-        TextView currentDay = (TextView) findViewById(R.id.current_date);
-        currentDay.setText(dateFormat.format(date));
 
-        db = new DBAdapter(this);
-        events = getEventsForDay();
     }
 
     @Override
@@ -57,30 +69,89 @@ public class DayView extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public ArrayList<Event> getEventsForDay(){
-        db.open();
-
-        ArrayList<Event> events = new ArrayList<>();
-
-        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    public void dateChange() {
+        currentDay = (EditText) findViewById(R.id.current_date);
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yy");
         Date date = new Date();
-        Cursor cursor = db.getRecordsForDate(dateFormat.format(date));
-
-        cursor.moveToFirst();
-
-        do {
-            Event event = new Event(cursor.getInt(0), cursor.getString(1), cursor.getString(2),
-                    cursor.getInt(3), cursor.getInt(4), cursor.getString(5));
-
-            events.add(event);
-
-            Toast.makeText(this, event.getName(), Toast.LENGTH_LONG ).show();
-        } while (cursor.moveToNext());
+        currentDay.setText(dateFormat.format(date));
+        eventList();
 
 
+        currentDay.setOnClickListener(new View.OnClickListener() {
 
-        return events;
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(DayView.this, dateToday, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            } // Show current date in date_picker.
+        });
     }
+
+    DatePickerDialog.OnDateSetListener dateToday = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateDateLabel();
+            eventList();
+        }
+
+    };
+
+    private void updateDateLabel() {
+
+        String dateFormat = "dd/MM/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+        currentDay.setText(sdf.format(myCalendar.getTime()));
+
+
+    }
+
+    public void eventList() {
+        db = new DBAdapter(this);
+        db.open();
+        String date = currentDay.getText().toString();
+        Cursor cursor = db.getRecordsForDate(date);
+        String[] fromFieldNames = new String[]{DBAdapter.EVENT_NAME, DBAdapter.START_TIME, DBAdapter.END_TIME, DBAdapter.NOTES};
+        int[] toViewIDs = new int[]{R.id.event_name, R.id.start_time, R.id.end_time, R.id.event_notes};
+        SimpleCursorAdapter myCursorAdapter = new SimpleCursorAdapter(this, R.layout.day_events, cursor, fromFieldNames, toViewIDs, 0);
+        listEvents = (ListView) findViewById(R.id.dayList);
+        listEvents.setAdapter(myCursorAdapter);
+
+        db.close();
+    }
+//    public ArrayList<Event>getEventsForDay(){
+//        db.open();
+//
+//        eventsToday = new ArrayList<Event>();
+//
+//        String date = currentDay.getText().toString();
+//        Cursor cursor = db.getRecordsForDate(date);
+//
+//        cursor.moveToFirst();
+//
+//        do {
+//            Event event = new Event();
+//            event.setId(Integer.parseInt(cursor.getString(0));
+//            event.setName(cursor.getString(1));
+//            event.setDate(cursor.getString(2));
+//            event.setStartTime(cursor.getString(3));
+//            event.setEndTime(cursor.getString(4));
+//            event.setNotes(cursor.getString(5));
+//
+//            //Adding Events to List
+//            eventsToday.add(event);
+//
+////            Toast.makeText(this, event.getName(), Toast.LENGTH_LONG ).show();
+//        } while (cursor.moveToNext());
+//
+//
+//
+//        return eventsToday;
+//    }
 
 }
 
